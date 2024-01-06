0b4190cc7ef166a3bf26aaf84a29a6e7b99fe9ab
matthieu777 <matthieu.diebolt@numericable.fr>
2024-01-06 15:24:16 +0100

    commentaires et moddif  de message.c

--- messages.c
+++ messages.c
@@ -18,14 +18,14 @@
 int send_file_entry(int msg_queue, int recipient, files_list_entry_t *file_entry, int cmd_code) {
     any_message_t msg; //utilisation du type defini 
     
-    msg.list_entry.reply_to = msg_queue;
-    msg.list_entry.mtype = recipient;
-    msg.list_entry.payload = *file_entry;
-    msg.list_entry.op_code = cmd_code;
-    
+    msg.list_entry.reply_to = msg_queue;//id d'un message
+    msg.list_entry.mtype = recipient;//type de message
+    msg.list_entry.payload = *file_entry; 
+    msg.list_entry.op_code = cmd_code; 
+    // au final cela utiliser la struct files_list_entry_transmit_t en passant par any_message_t  
 
     int result = msgsnd(msg_queue, &msg, sizeof(any_message_t) - sizeof(long), 0); //envoie du message
-     if (result == -1) {
+     if (result == -1) {    //gestion d'erreur
         printf("erreur\n");
     }
 
@@ -43,15 +43,15 @@ int send_analyze_dir_command(int msg_queue, int recipient, char *target_dir) {
 
     any_message_t msg;
     msg.analyze_dir_command.mtype = recipient;   
-    msg.analyze_dir_command.op_code = COMMAND_CODE_ANALYZE_DIR;
-    
+    msg.analyze_dir_command.op_code = COMMAND_CODE_ANALYZE_DIR; //indique qu'il faut analyser le directory
+    //cela reviens a utiliser la struct analyze_dir_command_t
 
-    strncpy(msg.analyze_dir_command.target, target_dir, PATH_SIZE - 1);
-    msg.analyze_dir_command.target[PATH_SIZE - 1] = '\0'; 
+    strncpy(msg.analyze_dir_command.target, target_dir, PATH_SIZE - 1);//copie du chemin
+    msg.analyze_dir_command.target[PATH_SIZE - 1] = '\0'; //pour etre sur que le chemin ce fini 
    
     int result = msgsnd(msg_queue, &msg, sizeof(analyze_dir_command_t) - sizeof(long), 0); // envoie du message
 
-    if (result == -1) {
+    if (result == -1) {     //gestion d'erreur
         printf("erreur\n");
     }
 
@@ -70,7 +70,7 @@ int send_analyze_dir_command(int msg_queue, int recipient, char *target_dir) {
  * Calls send_file_entry function
  */
 int send_analyze_file_command(int msg_queue, int recipient, files_list_entry_t *file_entry) {
-    return send_file_entry(msg_queue, recipient, file_entry, COMMAND_CODE_ANALYZE_FILE);
+    return send_file_entry(msg_queue, recipient, file_entry, COMMAND_CODE_ANALYZE_FILE); //COMMAND_CODE_ANALYZE_FILE indique qu'on doit analyser le fichier
 }
 
 /*!
@@ -82,7 +82,7 @@ int send_analyze_file_command(int msg_queue, int recipient, files_list_entry_t *
  * Calls send_file_entry function
  */
 int send_analyze_file_response(int msg_queue, int recipient, files_list_entry_t *file_entry) {
-    return send_file_entry(msg_queue, recipient, file_entry, COMMAND_CODE_FILE_ANALYZED);
+    return send_file_entry(msg_queue, recipient, file_entry, COMMAND_CODE_FILE_ANALYZED);  //COMMAND_CODE_FILE_ANALYZED indique que le ficheir envoyer est analyser
 }
 
 /*!
@@ -94,7 +94,7 @@ int send_analyze_file_response(int msg_queue, int recipient, files_list_entry_t 
  * Calls send_file_entry function
  */
 int send_files_list_element(int msg_queue, int recipient, files_list_entry_t *file_entry) {
-    return send_file_entry(msg_queue, recipient, file_entry, COMMAND_CODE_LIST_COMPLETE);
+    return send_file_entry(msg_queue, recipient, file_entry, COMMAND_CODE_FILE_ENTRY ); //COMMAND_CODE_FILE_ENTRY  indique que'il s'agit d'un files list entry
 }
 
 /*!
@@ -106,12 +106,12 @@ int send_files_list_element(int msg_queue, int recipient, files_list_entry_t *fi
 int send_list_end(int msg_queue, int recipient) {
     any_message_t msg;
     msg.simple_command.mtype = recipient;
-    msg.simple_command.message = COMMAND_CODE_LIST_COMPLETE;
-
-    int result = msgsnd(msg_queue, &msg, sizeof(simple_command_t) - sizeof(long), 0);
+    msg.simple_command.message = COMMAND_CODE_LIST_COMPLETE;// indique que c'est la fin puisques la list est complete
+    //reviens a utiliser la struct simple_command_t
+    int result = msgsnd(msg_queue, &msg, sizeof(simple_command_t) - sizeof(long), 0);//envoie du message
     
     if (result == -1) {
-        printf("erreur");
+        printf("erreur");   //cas d'echec
     }
 
     return result;
@@ -127,12 +127,12 @@ int send_list_end(int msg_queue, int recipient) {
 int send_terminate_command(int msg_queue, int recipient) {
     any_message_t msg;
     msg.simple_command.mtype = recipient;
-    msg.simple_command.message = COMMAND_CODE_TERMINATE;
-
-    int result = msgsnd(msg_queue, &msg, sizeof(simple_command_t) - sizeof(long), 0);
+    msg.simple_command.message = COMMAND_CODE_TERMINATE;//inquique que c'est un terminate command
+    //reviens a utiliser la struct simple_command_t
+    int result = msgsnd(msg_queue, &msg, sizeof(simple_command_t) - sizeof(long), 0);//envoie du message
 
     if (result == -1) {
-        printf("erreur \n");
+        printf("erreur \n");    //cas d'erreur
     }
 
     return result;
@@ -147,12 +147,12 @@ int send_terminate_command(int msg_queue, int recipient) {
 int send_terminate_confirm(int msg_queue, int recipient) {
     any_message_t msg;
     msg.simple_command.mtype = recipient;
-    msg.simple_command.message = COMMAND_CODE_TERMINATE_OK;
+    msg.simple_command.message = COMMAND_CODE_TERMINATE_OK;//indique que c'est un terminate confiramation
 
-    int result = msgsnd(msg_queue, &msg, sizeof(simple_command_t) - sizeof(long), 0);
+    int result = msgsnd(msg_queue, &msg, sizeof(simple_command_t) - sizeof(long), 0); //envoie message
 
     if (result == -1) {
-        printf("erreur");
+        printf("erreur");   //cas d'erreur
     }
 
     return result;
