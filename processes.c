ba46db794796035f4524e35cdfd6e72227a8fb3f
matthieu777 <matthieu.diebolt@numericable.fr>
2024-01-06 16:08:24 +0100

    make_process et clean processes de processe.c

--- processes.c
+++ processes.c
@@ -3,12 +3,13 @@
 #include <unistd.h>
 #include <sys/msg.h>
 #include <stdio.h>
-#include <messages.h>
-#include <file-properties.h>
-#include <sync.h>
+#include "messages.h"
+#include "file-properties.h"
+#include "sync.h"
 #include <string.h>
 #include <errno.h>
 
+#include <sys/wait.h>
 /*!
  * @brief prepare prepares (only when parallel is enabled) the processes used for the synchronization.
  * @param the_config is a pointer to the program configuration
@@ -44,6 +45,20 @@ int prepare(configuration_t *the_config, process_context_t *p_context) {
  * @return the PID of the child process (it never returns in the child process)
  */
 int make_process(process_context_t *p_context, process_loop_t func, void *parameters) {
+     pid_t child_pid = fork(); // creation du processus enfant
+
+     if (child_pid == -1){
+        printf("errreur lors de la creation du processus");
+        exit(EXIT_FAILURE);
+     } else if ( child_pid == 0){
+        func(parameters); // appel de la fonction pour le processus enfant 
+        exit(EXIT_SUCCESS); // le processus enfant ce fini
+     } else {
+        p_context->processes_count++; //on ajout 1 au nombre de processus
+        return child_pid; //donne le pid du processus enfant au parent 
+     }
+
+
 }
 
 /*!
@@ -67,8 +82,30 @@ void analyzer_process_loop(void *parameters) {
  */
 void clean_processes(configuration_t *the_config, process_context_t *p_context) {
     // Do nothing if not parallel
+     if (!the_config->is_parallel) {
+        return;
+    }
     // Send terminate
+
+     for (int i = 0; i < p_context->processes_count; ++i) { //boucle pour récuprer chaque pid
+        int pid = p_context->destination_analyzers_pids[i]; //recupere le pid stocker
+        if (pid > 0) { //si c'est un pid valide
+            send_terminate_command(p_context->message_queue_id, pid); //appel de la fonction pour send terminate
+        }
+    }
+
     // Wait for responses
+
+    for (int i = 0; i < p_context->processes_count; ++i) { //boucle pour récuprer chaque pid
+        int pid = p_context->destination_analyzers_pids[i];//recupere le pid stocker
+        if (pid > 0) {  //si c'est un pid valide
+            int status; //pour recuperer le statut du processus fini
+            waitpid(pid, &status, 0);//attente que le pid ce fini
+        }
+    }
+
     // Free allocated memory
-    // Free the MQ
+    free(p_context->source_analyzers_pids);
+     // Free the MQ
+    msgctl(p_context->message_queue_id, IPC_RMID,NULL);
 }
